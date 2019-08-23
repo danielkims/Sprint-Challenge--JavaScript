645ce24ef6ebef7f6adec8d3d1e010addf11da05
Daniel <danielkim.s@outlook.com>
2019-08-23 08:59:55 -0700

    Started objects-array part of the Sprint Challenge

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,10 +7,31 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyranno = {
+  dinoName: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous"
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stego = {
+  dinoName: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9g",
+  period: "Late Jurassic"
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velo = {
+  dinoName: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous"
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
