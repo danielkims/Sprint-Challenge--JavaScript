705673fab38d76f1f55cce661c17119696a4ed7f
Daniel <danielkim.s@outlook.com>
2019-08-23 12:42:11 -0700

    Finished Advanced Array Methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -134,7 +134,7 @@ for (abv in uni) {
     Uni.push(uni[abv].university)
   }
 }
-console.log(Uni);
+console.log(unisWithUni);
 
 
 // ==== ADVANCED Array Methods ====
@@ -160,6 +160,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach( sci => {
+  displayNames.push(`${sci.animal_name} ${sci.scientific_name}`)
+})
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -169,6 +172,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map( lc => {
+  lowCaseAnimalNames.push(`${lc.animal_name.toLowerCase()}`)
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -176,7 +182,9 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter((lowpop) => {
+  return lowpop.population < 5
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -184,7 +192,9 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((populationTotal, populationAmount) => {
+  return populationTotal + populationAmount.population
+}, 0)
 console.log(populationTotal);
 
 
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -40,7 +40,7 @@ const Cuboid = new CuboidMaker({
 })
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(Cuboid.volume()); // 100
+console.log(Cuboid.surfaceArea()); // 130
 
 
